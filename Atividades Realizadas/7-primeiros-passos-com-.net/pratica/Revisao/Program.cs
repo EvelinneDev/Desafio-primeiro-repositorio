52d4bd0f290dc0b90aa2fc370febb0904a591d19
EvelinneDev <95060483+EvelinneDev@users.noreply.github.com>
2021-12-13 16:47:52 -0300

    Atualizando aulas 7 e 8

---
+++ Atividades Realizadas/7-primeiros-passos-com-.net/pratica/Revisao/Program.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace Revisao
+{
+   class Program
+   {
+       static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X");
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+                     
+                        Console.WriteLine("Informe a nota do aluno:");
+                        
+                        var nota = decimal.Parse(Console.ReadLine());
+                        
+                        aluno.Nota = nota;
+
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        break;
+                    case "3":
+                        //TODO: Calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+        }
+    }
+    
+}
